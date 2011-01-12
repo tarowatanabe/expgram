7c2155dc0eb27b8d9f3d8fb8a0843a97f29beb0a
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-01-12 16:25:25 +0900

    bugfix from cicada...

--- utils/base64.hpp
+++ utils/base64.hpp
@@ -1,6 +1,6 @@
 // -*- mode: c++ -*-
 //
-//  Copyright(C) 2010 Taro Watanabe <taro.watanabe@nict.go.jp>
+//  Copyright(C) 2010-2011 Taro Watanabe <taro.watanabe@nict.go.jp>
 //
 
 #ifndef __UTILS__BASE64__HPP__
@@ -8,6 +8,9 @@
 
 #include <string>
 #include <algorithm>
+#include <iterator>
+
+#include <utils/bithack.hpp>
 
 #include <boost/archive/iterators/binary_from_base64.hpp>
 #include <boost/archive/iterators/base64_from_binary.hpp>
@@ -15,19 +18,60 @@
 
 namespace utils
 {
+  template <typename Tp, typename Iterator>
+  inline
+  Iterator encode_base64(const Tp& x, Iterator iter)
+  {
+    static const char* enc64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    
+    const unsigned char* buf = reinterpret_cast<const unsigned char*>(&x);
+    const size_t size = sizeof(Tp);
+    
+    size_t curr = 0;
+    while (curr < size) {
+      const size_t len = utils::bithack::min(size - curr, size_t(3));
+      
+      *iter = enc64[buf[curr + 0] >> 2]; ++ iter;
+      
+      switch (len) {
+      case 3:
+	*iter = enc64[((buf[curr + 0] & 0x03) << 4) | ((buf[curr + 1] & 0xf0) >> 4)]; ++ iter;
+	*iter = enc64[((buf[curr + 1] & 0x0f) << 2) | ((buf[curr + 2] & 0xc0) >> 6)]; ++ iter;
+	*iter = enc64[buf[curr + 2] & 0x3f];                                          ++ iter;
+	break;
+      case 2:
+	*iter = enc64[((buf[curr + 0] & 0x03) << 4) | ((buf[curr + 1] & 0xf0) >> 4)]; ++ iter;
+	*iter = enc64[(buf[curr + 1] & 0x0f) << 2];                                   ++ iter;
+	break;
+      case 1:
+	*iter = enc64[(buf[curr + 0] & 0x03) << 4]; ++ iter;
+	break;
+      }
+      
+      curr += len;
+    }
+
+    return iter;
+    
+#if 0
+    using namespace boost::archive::iterators;
+    
+    typedef base64_from_binary<transform_width<const char*, 6, 8> > encoder_type;
+    
+    std::copy(encoder_type((const char*) &x), encoder_type(((const char*) &x) + sizeof(Tp)), iter);
+    return iter;
+#endif
+  }
+
   template <typename Tp>
   inline
   std::string encode_base64(const Tp& x)
   {
-    using namespace boost::archive::iterators;
-
-    typedef base64_from_binary<transform_width<const char*, 6, 8> > encoder_type;
-
     std::string encoded;
-    std::copy(encoder_type((const char*) &x), encoder_type(((const char*) &x) + sizeof(x)), std::back_inserter(encoded));
-  
+    encode_base64(x, std::back_inserter(encoded));
     return encoded;
   }
+
 
   template <typename Tp>
   inline
