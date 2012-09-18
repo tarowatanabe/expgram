57f5650971ae75681e6f9ce3e6125f975bb2173f
Taro Watanabe <taro.watanabe@nict.go.jp>
2012-09-19 08:11:32 +0900

    better directory finding

--- scripts/expgram.py
+++ scripts/expgram.py
@@ -216,27 +216,24 @@ class MPI:
 
 class Expgram:
     def __init__(self, dir=""):
-        self.bindirs = []
+        bindirs = []
         
         if not dir:
             dir = os.path.abspath(os.path.dirname(__file__))
-            if dir:
-                self.bindirs.append(dir)
-                
-                dir = os.path.dirname(dir)
-                if dir:
-                    self.bindirs.append(dir)
+            bindirs.append(dir)
+            parent = os.path.dirname(dir)
+            if parent:
+                dir = parent
+        else:
+            dir = os.path.realpath(dir)
+            if not os.path.exists(dir):
+                raise ValueError, dir + " does not exist"
+            bindirs.append(dir)
         
-	self.dir = dir
-	if not os.path.exists(self.dir):
-	    raise ValueError, self.dir + " does not exist"
-	self.dir = os.path.realpath(self.dir)
-
-	for dir in ('bin', 'progs', 'scripts'): 
-	    bindir = os.path.join(self.dir, dir)
+	for subdir in ('bin', 'progs', 'scripts'): 
+	    bindir = os.path.join(dir, subdir)
 	    if os.path.exists(bindir) and os.path.isdir(bindir):
-		self.bindirs.append(bindir)
-        self.bindirs.append(self.dir)
+		bindirs.append(bindir)
 	
         for binprog in (## vocabulary
                         'expgram_vocab', 'expgram_vocab_mpi',
@@ -251,7 +248,7 @@ class Expgram:
                         'expgram_bound',    'expgram_bound_mpi',
                         'expgram_quantize', 'expgram_quantize_mpi',):
 	    
-	    for bindir in self.bindirs:
+	    for bindir in bindirs:
 		prog = os.path.join(bindir, binprog)
                 
                 if not os.path.exists(prog): continue
