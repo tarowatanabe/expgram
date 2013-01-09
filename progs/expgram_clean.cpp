e3de23a20e5dfa85e91a44402a32f118bdfbe9f7
Taro Watanabe <taro.watanabe@nict.go.jp>
2013-01-09 10:57:47 +0900

    updated expgram-clean to accept temporary option

--- progs/expgram_clean.cpp
+++ progs/expgram_clean.cpp
@@ -4,17 +4,29 @@
 
 // clear temporary files
 
+#include <cstdlib>
+
 #include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 #include <boost/algorithm/string/find.hpp>
 
 #include <utils/tempfile.hpp>
 #include <utils/filesystem.hpp>
 
 typedef boost::filesystem::path path_type;
+typedef boost::program_options::variables_map variable_set_type;
+
+void options(int argc, char** argv, variable_set_type& variables);
 
 int main(int argc, char** argv)
 {
   try {
+    variable_set_type variables;
+    options(argc, argv, variables);
+    
+    if (variables.count("temporary") && ! variables["temporary"].as<path_type>().empty())
+      ::setenv("TMPDIR_SPEC", variables["temporary"].as<path_type>().string().data(), 1);
+    
     const path_type tmp_dir = utils::tempfile::tmp_dir();
 
     static const char* expgram_suffix[9] = {
@@ -60,4 +72,25 @@ int main(int argc, char** argv)
     return -1;
   }
   return 0;
+}
+
+void options(int argc, char** argv, variable_set_type& variables)
+{
+  namespace po = boost::program_options;
+  
+  po::options_description desc("options");
+  desc.add_options()
+    ("temporary", po::value<path_type>(), "temporary directory")
+    
+    ("help", "help message");
+
+  po::store(po::parse_command_line(argc, argv, desc, po::command_line_style::unix_style & (~po::command_line_style::allow_guessing)), variables);
+  
+  po::notify(variables);
+  
+  if (variables.count("help")) {
+    std::cout << argv[0] << " [options]\n"
+	      << desc << std::endl;
+    exit(0);
+  }
 }
