1e27289cbc5ad272a82b12986202d5e9d284f45f
Taro Watanabe <taro.watanabe@nict.go.jp>
2013-04-09 11:32:05 +0900

    simplified ngram example

--- sample/ngram.cpp
+++ sample/ngram.cpp
@@ -62,33 +62,63 @@ int main(int argc, char** argv)
     while (std::getline(std::cin, line)) {
       tokenizer_type tokenizer(line);
       
-      // here, we store in vector<string>.
-      // alternatively you can try: (Remark: sentence_type is simply vector<word_type>)
-      // 
-      // sentence.clear();
-      // sentence.push_back(vocab_type::BOS);
-      // sentence.insert(sentence.end(), tokenizer.begin(), tokenizer.end());
-      // sentence.push_back(vocab_type::EOS);
-      //
-      // and iterate over sentence type...
-      //
-      // or even more: (Remark: we assume id_set is vector<word_type::id_type> )
-      //
-      // id_set.clear();
-      // id_set.push_back(ngram.index.vocab()[vocab_type::BOS]);
-      // for (tokenizer_type::iterator titer = tokenizer.begin(); titer != tokenizer.end(); ++ titer)
-      //   id_set.push_back(ngram.index.vocab()[*titer]);
-      // id_set.push_back(ngram.index.vocab()[vocab_type::EOS]);
-      //
-      // then, you can iterate over id_set
+      // Here, we store in vector<string>.
       
       tokens.clear();
       tokens.push_back(vocab_type::BOS);
       tokens.insert(tokens.end(), tokenizer.begin(), tokenizer.end());
       tokens.push_back(vocab_type::EOS);
 
+      //
+      // Alternatively, you can try: (Remark: sentence is simply vector<word_type>)
+      // 
+      // sentence.clear();
+      // sentence.push_back(vocab_type::BOS);
+      // sentence.insert(sentence.end(), tokenizer.begin(), tokenizer.end());
+      // sentence.push_back(vocab_type::EOS);
+      //
+      // and iterate over sentence type, not tokens.
+      //
+      
+      //
+      // The above example still automatically convert word_type into word_type::id_type on the fly.
+      // An alternative faster approach is: (Remark: we assume id_set is vector<word_type::id_type> )
+      //
+      // const word_type::id_type bos_id = ngram.index.vocab()[vocab_type::BOS]
+      // const word_type::id_type eos_id = ngram.index.vocab()[vocab_type::EOS]
+      //
+      // id_set.clear();
+      // id_set.push_back(bos_id);
+      // for (tokenizer_type::iterator titer = tokenizer.begin(); titer != tokenizer.end(); ++ titer)
+      //   id_set.push_back(ngram.index.vocab()[*titer]);
+      // id_set.push_back(eos_id);
+      //
+      // then, you can iterate over id_set, not tokens.
+      // 
+      
+      // 
+      // Note that the word_type will automatically assign id which may not
+      // match with the word-id assigned by the indexed ngram language model.
+      // This means that even OOV by the ngram language model may be assigned word-id.
+      // If you want to avoid this, here is a solution:
+      //
+      //
+      // const word_type::id_type bos_id = ngram.index.vocab()[vocab_type::BOS]
+      // const word_type::id_type eos_id = ngram.index.vocab()[vocab_type::EOS]
+      // const word_type::id_type unk_id = ngram.index.vocab()[vocab_type::UNK]
+      //
+      // id_set.clear();
+      // id_set.push_back(bos_id);
+      // for (tokenizer_type::iterator titer = tokenizer.begin(); titer != tokenizer.end(); ++ titer)
+      //   id_set.push_back(ngram.index.vocab().exists(*titer) ? ngram.index.vocab()[*titer] : unk_id);
+      // id_set.push_back(eos_id);
+      //
+      
+      // A state-based example...
+      // Since will not score P(BOS), state should starts from BOS!
       state_type state = ngram.index.next(state_type(), vocab_type::BOS);
-
+      
+      // debug message...
       std::cerr << "bos state: shard: " <<  state.shard()
 		<< " node: " << state.node()
 		<< " is-root-shard: " << state.is_root_shard()
@@ -100,11 +130,9 @@ int main(int argc, char** argv)
       tokens_type::const_iterator titer_end = tokens.end();
       for (tokens_type::const_iterator titer = titer_begin + 1; titer != titer_end; ++ titer) {
 	
-	// ngram access must use containser that supports random-iterator concepts.
-	// If not sure, use vector!
-
-	std::pair<state_type, float> result_logprob  = ngram.logprob(state, *titer);	
+	std::pair<state_type, float> result_logprob  = ngram.logprob(state, *titer);
 	
+	// some debug information...
 	std::cerr << "state: shard: " <<  result_logprob.first.shard()
 		  << " node: " << result_logprob.first.node()
 		  << " is-root-shard: " << result_logprob.first.is_root_shard()
@@ -113,7 +141,14 @@ int main(int argc, char** argv)
 		  << " base10: " << (result_logprob.second / log_10)
 		  << " order: " << ngram.index.order(result_logprob.first)
 		  << std::endl;
-		
+	
+	// next state!
+	state = result_logprob.first;
+	
+	// non-state version for scoring ngrams
+	// ngram access must use container that supports random-iterator concepts.
+	// If not sure, use vector!
+	
 	tokens_type::const_iterator titer_first = std::max(titer_begin, titer + 1 - order);
 	tokens_type::const_iterator titer_last  = titer + 1;
 	
@@ -126,54 +161,48 @@ int main(int argc, char** argv)
 	if (score_logprob != result_logprob.second)
 	  std::cerr << "logprob differ: " << score_logprob << ' ' << result_logprob.second << std::endl;
 	
+	// we compute prefix for decoding with CKY-style bottom up algorithm.
+	// The prefix is the left context which should be scored when "correct" history is available...
 	std::pair<tokens_type::const_iterator, tokens_type::const_iterator> prefix = ngram.prefix(titer_first, titer_last);
-
+	
 	std::cout << "prefix: ";
 	std::copy(prefix.first, prefix.second, std::ostream_iterator<std::string>(std::cout, " "));
 	std::cout << std::endl;
 	
-	state = result_logprob.first;
+	// An example for upper bound estimates, which is used to compute heursitic estimates.
+	// Usually, we will score the ngram "prefix" computed by ngram.prefix(...)
+	state_type state_ngram;
 	
-	{
-	  state_type state;
+	for (tokens_type::const_iterator titer = titer_first; titer != titer_last; ++ titer) {
+	  // state-based access...
+	  const bool backoffed = ngram.index.order(state_ngram) != std::distance(titer_first, titer);
 	  
-	  for (tokens_type::const_iterator titer = titer_first; titer != titer_last; ++ titer) {
-	    const float score_logprob  = ngram.logprob(titer_first, titer + 1);
-	    const float score_logbound = ngram.logbound(titer_first, titer + 1);
-	    
-	    const bool backoffed = ngram.index.order(state) != std::distance(titer_first, titer);
-	    
-	    const std::pair<state_type, float> result_logprob  = ngram.logprob(state, *titer, backoffed, order);
-	    const std::pair<state_type, float> result_logbound = ngram.logbound(state, *titer, backoffed, order);
-	    
-	    std::cout << "\t";
-	    std::copy(titer_first, titer + 1, std::ostream_iterator<std::string>(std::cout, " "));
-	    std::cout << std::endl;
-	    
-	    std::cerr << "\tstate: shard: " <<  result_logprob.first.shard()
-		      << " node: " << result_logprob.first.node()
-		      << " is-root-shard: " << result_logprob.first.is_root_shard()
-		      << " is-root-node: " << result_logprob.first.is_root_node()
-		      << " logprob: " << result_logprob.second
-		      << " base10: " << (result_logprob.second / log_10)
-		      << " logbound: " << result_logbound.second
-		      << " order: " << ngram.index.order(result_logprob.first)
-		      << std::endl;
-
-	    
-	    if (result_logprob.first != result_logbound.first)
-	      std::cerr << "\tstate differ:" << std::endl
-			<< "\tlogprob:  shard: " << result_logprob.first.shard() << " node: " << result_logprob.first.node() << std::endl
-			<< "\tlogbound: shard: " << result_logbound.first.shard() << " node: " << result_logbound.first.node() << std::endl;
-	    
-	    
-	    if (score_logprob != result_logprob.second)
-	      std::cerr << "\tlogprob differ: " << score_logprob << ' ' << result_logprob.second << std::endl;
-	    if (score_logbound != result_logbound.second)
-	      std::cerr << "\tlogbound differ: " << score_logbound << ' ' << result_logbound.second << std::endl;
-	    
-	    state = result_logprob.first;
-	  }
+	  const std::pair<state_type, float> result_logprob  = ngram.logprob(state_ngram, *titer, backoffed, order);
+	  const std::pair<state_type, float> result_logbound = ngram.logbound(state_ngram, *titer, backoffed, order);
+	  
+	  // next state...
+	  state_ngram = result_logprob.first;
+	  
+	  // non-state-based access...
+	  const float score_logprob  = ngram.logprob(titer_first, titer + 1);
+	  const float score_logbound = ngram.logbound(titer_first, titer + 1);
+	  
+	  std::cout << result_logprob.second
+		    << ' ' << result_logbound.second
+		    << '\t';
+	  std::copy(titer_first, titer + 1, std::ostream_iterator<std::string>(std::cout, " "));
+	  std::cout << std::endl;
+	  
+	  // they should be equal!
+	  if (result_logprob.first != result_logbound.first)
+	    std::cerr << "\tstate differ:" << std::endl
+		      << "\tlogprob:  shard: " << result_logprob.first.shard() << " node: " << result_logprob.first.node() << std::endl
+		      << "\tlogbound: shard: " << result_logbound.first.shard() << " node: " << result_logbound.first.node() << std::endl;
+	  
+	  if (score_logprob != result_logprob.second)
+	    std::cerr << "\tlogprob differ: " << score_logprob << ' ' << result_logprob.second << std::endl;
+	  if (score_logbound != result_logbound.second)
+	    std::cerr << "\tlogbound differ: " << score_logbound << ' ' << result_logbound.second << std::endl;
 	}
       }
     }
