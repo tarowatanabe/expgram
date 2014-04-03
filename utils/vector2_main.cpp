749c79b3ea9874074d875ec0a89281fba34cc7cb
Taro Watanabe <taro.watanabe@nict.go.jp>
2014-04-03 21:16:44 +0900

    removed unused files

--- utils/vector2_main.cpp
+++ utils/vector2_main.cpp
@@ -6,16 +6,11 @@
 #include "utils/vector2.hpp"
 #include "utils/vector3.hpp"
 
-#include "utils/vector2_aligned.hpp"
-#include "utils/vector3_aligned.hpp"
 
 int main(int argc, char** argv)
 {
   utils::vector2<int> vec2;
   utils::vector3<int> vec3;
-
-  utils::vector2_aligned<int> vec2_aligned;
-  utils::vector3_aligned<int> vec3_aligned;
 
   srandom(time(0) * getpid());
   
@@ -35,9 +30,6 @@ int main(int argc, char** argv)
   
     vec2.resize(dim1, dim2);
     vec3.resize(dim1, dim2, dim3);
-
-    vec2_aligned.resize(dim1, dim2);
-    vec3_aligned.resize(dim1, dim2, dim3);
   
     for (int i = 0; i < 1000; ++ i) {
       const int pos1 = random() % dim1;
@@ -50,19 +42,9 @@ int main(int argc, char** argv)
 	std::cerr << "DIFFER..." << std::endl;
       if (&(vec2(pos1, pos2)) != &(vec2[pos1][pos2]))
 	std::cerr << "DIFFER..." << std::endl;
-      
-      if (&(vec2_aligned(pos1, 0)) != &(*vec2_aligned.begin(pos1)))
-	std::cerr << "DIFFER..." << std::endl;
-      if (&(vec2_aligned(pos1, pos2)) != &(*(vec2_aligned.begin(pos1) + pos2)))
-	std::cerr << "DIFFER..." << std::endl;
-      if (&(vec2_aligned(pos1, pos2)) != &(vec2_aligned[pos1][pos2]))
-	std::cerr << "DIFFER..." << std::endl;
-      
+            
       for (int pos = 0; pos < dim2; ++ pos) {
 	if (&(vec2(pos1, pos)) != &(vec2[pos1][pos]))
-	  std::cerr << "DIFFER..." << std::endl;
-	
-	if (&(vec2_aligned(pos1, pos)) != &(vec2_aligned[pos1][pos]))
 	  std::cerr << "DIFFER..." << std::endl;
       }
       
@@ -71,13 +53,6 @@ int main(int argc, char** argv)
       if (&(vec3(pos1, pos2, 0)) != &(*(vec3.begin(pos1, pos2))))
 	std::cerr << "DIFFER..." << std::endl;
       if (&(vec3(pos1, pos2, pos3)) != &(*(vec3.begin(pos1, pos2) + pos3)))
-	std::cerr << "DIFFER..." << std::endl;
-      
-      if (&(vec3_aligned(pos1, 0, 0)) != &(*vec3_aligned.begin(pos1)))
-	std::cerr << "DIFFER..." << std::endl;
-      if (&(vec3_aligned(pos1, pos2, 0)) != &(*(vec3_aligned.begin(pos1, pos2))))
-	std::cerr << "DIFFER..." << std::endl;
-      if (&(vec3_aligned(pos1, pos2, pos3)) != &(*(vec3_aligned.begin(pos1, pos2) + pos3)))
 	std::cerr << "DIFFER..." << std::endl;
     }
   }
